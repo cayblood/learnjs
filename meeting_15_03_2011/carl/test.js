a84c136a370208b3591bf7be96a8687ef2223c24
Stig Murberg <stimurbe@cisco.com>
2011-03-21 20:10:27 +0100

    moved some files

--- meeting_15_03_2011/carl/test.js
+++
@@ -1,8 +0,0 @@
-function builder(name, value, base) {
-  var real_base = base || {};
-  real_base[name] = value;
-  return real_base;
-}
-
-var abcd = builder("c", "d", builder("a", "b"));
-console.log(abcd.a);
