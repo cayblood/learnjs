7ba21b7cc3da21adf1e9d5100fb94268d3e30bcf
Jon Tore Hafstad <jhafstad@cisco.com>
2011-04-12 16:55:24 +0200

    Testing arrays

---
+++ meetings/5/jth/ex1.js
@@ -0,0 +1,14 @@
+var empty = [];
+
+empty[0] = 1;
+empty[1] = 2;
+empty[2] = 3;
+
+var numbers = [
+  'zero', 'one', 'two', 'three', 'four',
+];
+
+console.log(empty.length);
+console.log(numbers.length);
+console.log(empty);
+console.log(numbers);
