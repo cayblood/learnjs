ad561cc6d6707fca9768228a5d709d3997f722aa
Stig Murberg <stimurbe@cisco.com>
2011-04-14 15:36:58 +0200

    more recursion awesomeness

--- meetings/5/smu/ex4.js
+++ meetings/5/smu/ex4.js
@@ -16,4 +16,31 @@ var crtr = function(x, y) {
   return arr;
 }
 
+console.log('- crtr v1 -');
 console.log(crtr(10, 10));
+
+var crtr2 = function() {
+  var args = [].slice.call(arguments);
+  var arr = typeof args[args.length -1] == 'object' ? args.pop() : [];
+  var num = args.shift();
+  if (arr !== num) {
+    fun(num, function(i) {
+      if (args.length) {
+        arr[i] = [];
+        var nargs = args.slice();
+        nargs.push(arr[i]);
+        crtr2.apply(this, nargs);
+      } else {
+        arr[i] = i % 2;
+      }
+    });
+  }
+
+  return arr;
+}
+
+console.log('- crtr v2 -');
+console.log(crtr2(10, 10));
+
+console.log('- awesome -');
+console.log(crtr2(10, 10, 10));
