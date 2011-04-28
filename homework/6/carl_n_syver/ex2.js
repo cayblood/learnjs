85d94a733cfa3aac91e0d68044d95fb9a0de92c8
Carl Youngblood <carl@youngbloods.org>
2011-04-28 18:18:28 +0200

    Added second assignment

---
+++ homework/6/carl_n_syver/ex2.js
@@ -0,0 +1,45 @@
+"use strict";
+
+function validatePart(input, regex) {
+  var parts = input.split('.');
+  for (var i = 0; i < parts.length; i++) {    
+    if (!regex.test(parts[i])) {
+      return false;
+    }
+  }
+  return true;  
+}
+
+function validLocalPart(input) {
+  var regex = /^[a-z0-9!#$%&'*+\-\/=?\^_`{|}~]+$/;
+  return validatePart(input, regex);
+}
+
+function validDomainPart(input) {
+  var regex = /^[a-z0-9\-]+$/;
+  return validatePart(input, regex);  
+}
+
+function validEmail(email) {
+  var parts = email.split('@');
+  if (parts.length !== 2) {
+    return false;
+  }  
+  return validLocalPart(parts[0]) && validDomainPart(parts[1]);
+}
+
+var test_email = "testur+garbage@testur.is";
+console.log(validEmail(test_email));
+
+var test_email = "carl.anthony.youngblood@cisco.no";
+console.log(validEmail(test_email));
+
+var test_email = "testur+garbage.second_part@testur.is";
+console.log(validEmail(test_email));
+
+var test_email = "testur+garbage@testur.bad_domain.com";
+console.log(validEmail(test_email));
+
+var test_email = "no..consecutive.dots@testur.is";
+console.log(validEmail(test_email));
+
